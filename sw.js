f85478f6e1b94346e5c90351ff07cb74c3ddaf4a
jian <newlifewj@gmail.com>
2021-12-01 01:27:25 -0500

    ff

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/pwademo/',
+        
+        '/pwademo/gallery/myLittleVader.jpg'
       ]);
     })
   );
