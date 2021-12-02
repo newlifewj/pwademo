e459074183e0469e38b975452c54992cff4e28a0
jian <newlifewj@gmail.com>
2021-12-01 20:05:44 -0500

    ss

--- sw.js
+++ sw.js
@@ -1,8 +1,14 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v11').then(function(cache) {
-      return cache.adderAll([
-        
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/pwademo/',
+        '/pwademo/index.html',
+        '/pwademo/style.css',
+        '/pwademo/app.js',
+        '/pwademo/image-list.js',
+        '/pwademo/star-wars-logo.jpg',
+        '/pwademo/gallery/bountyHunters.jpg',
         '/pwademo/gallery/myLittleVader.jpg'
       ]);
     })
@@ -10,28 +16,26 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log(`---------001-----${event.request.url}`)
+  console.log(`---------------${event.request.url}`)
   event.respondWith(caches.match(event.request).then(function(response) {
     // caches.match() always resolves
     // but in case of success response will have value
     if (response !== undefined) {
-      console.log(`---------111-----${event.request.url}`)
       return response;
     } else {
-    console.log(`---------222-----${event.request.url}`)
-      return fetch(event.request).then(function (response) {
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        let responseClone = response.clone();
-        
-        caches.open('v11').then(function (cache) {
-          cache.put(event.request, responseClone);
+        return fetch(event.request).then(function (response) {
+          // response may be used only once
+          // we need to save clone to put one copy in cache
+          // and serve second one
+          let responseClone = response.clone();
+          
+          caches.open('v1').then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        }).catch(function () {
+          return caches.match('/pwademo/gallery/myLittleVader.jpg');
         });
-        return response;
-      }).catch(function () {
-        return caches.match('/pwademo/gallery/myLittleVader.jpg');
-      });
-    }
-  }));
+      }
+    }));
 });
