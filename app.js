b2ba482eabbef3a7654cf19b124dd4ce8e8048b3
jian <newlifewj@gmail.com>
2021-12-01 20:40:02 -0500

    ff

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // register service worker
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/pwademo/gallery/sw.js', { scope: '/pwademo/gallery/' }).then(function(reg) {
+  navigator.serviceWorker.register('/pwademo/gallery/sw.js').then(function(reg) {
 
     if(reg.installing) {
       console.log('Service worker installing');
